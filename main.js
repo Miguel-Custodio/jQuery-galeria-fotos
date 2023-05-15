028e36e821fcdf61f15fb130c3f39badfafba334
Miguel Custodio <126588181+Miguel-Custodio@users.noreply.github.com>
2023-05-15 15:51:41 -0300

    Commit inicial

--- main.js
+++ main.js
@@ -1,27 +1,46 @@
 $(document).ready(function() {
-    $('header button').click(function() {
-        $('form').slideDown();
-    })
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00.000-000')
+    });
 
-    $('#botao-cancelar').click(function() {
-        $('form').slideUp()
-    })
-
-    $('form').on('submit', function(e) {
-        e.preventDefault();
-        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-        console.log(enderecoDaNovaImagem);
-        const novoItem =$('<li></li>');
-        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
-        $(`
-            <div class="overlay-imagem-link">
-                <a href="${enderecoDaNovaImagem}" target="blank" title="Ver imgaem em tamanho real">
-                    Ver imagem e tamanho real
-                </a>
-            </div>
-        `).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn(1000);
-        $('#endereco-imagem-nova').val('');
-    })
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            CPF:  {
+            required: true,
+            },
+            endereco: {
+                required:true,
+            },
+            CEP: {
+                required:true,
+            },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            email: 'Por favor, insira um email válido',
+            telefone: 'Por favor, insira um número de telefone',
+            CPF: 'Por favor, insira um CPF válido',
+            endereco: 'Por favor, insira o seu endereço',
+            CEP: 'Por favor, insira um CEP válido'
+        },
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
 })
